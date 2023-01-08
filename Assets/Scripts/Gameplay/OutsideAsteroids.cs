358b1142740ec62861a7ea5324c27a033208c766
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2023-01-08 18:56:51 +0100

    ...

---
+++ Assets/Scripts/Gameplay/OutsideAsteroids.cs
@@ -0,0 +1,12 @@
+﻿using Asteroids;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class OutsideAsteroids
+    {
+        [SerializeField] private AsteroidFactory _asteroidFactory;
+        [SerializeField] private BoxCollider _spawnBox;
+
+    }
+}

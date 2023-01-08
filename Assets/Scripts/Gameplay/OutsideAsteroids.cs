57a8b514e642b397536dd8b98c95c37902757a64
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2023-01-08 19:19:40 +0100

    ...

--- Assets/Scripts/Gameplay/OutsideAsteroids.cs
+++ Assets/Scripts/Gameplay/OutsideAsteroids.cs
@@ -1,12 +1,50 @@
-﻿using Asteroids;
+﻿using System.Collections.Generic;
+using Asteroids;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Gameplay
 {
-    public class OutsideAsteroids
+    public class OutsideAsteroids : MonoBehaviour
     {
         [SerializeField] private AsteroidFactory _asteroidFactory;
         [SerializeField] private BoxCollider _spawnBox;
+        [SerializeField] private int _targetAsteroidCount;
+        private List<Asteroid> _asteroids;
 
+        private void Start()
+        {
+            _asteroids = SpawnFirstAsteroids();
+        }
+
+        private List<Asteroid> SpawnFirstAsteroids()
+        {
+            List<Asteroid> result = new List<Asteroid>();
+            for (int i = 0; i < _targetAsteroidCount; i++)
+            {
+                result.Add(SpawnAsteroid());
+            }
+            return result;
+        }
+
+        private Asteroid SpawnAsteroid()
+        {
+            Asteroid asteroid = _asteroidFactory.BuildAsteroid();
+            Rigidbody asteroidRigidbody = asteroid.GetRigidbody();
+            asteroidRigidbody.useGravity = false;
+            asteroidRigidbody.position = PickPointInBounds();
+            asteroidRigidbody.velocity = (asteroidRigidbody.position - PickPointInBounds()).normalized * Random.Range(0f, 2f);
+            return asteroid;
+        }
+
+        private Vector3 PickPointInBounds()
+        {
+            Bounds bounds = _spawnBox.bounds;
+            return new Vector3(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y),
+                Random.Range(bounds.min.z, bounds.max.z)
+            );
+        }
     }
 }

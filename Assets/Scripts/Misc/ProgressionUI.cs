6a967d65200a564f05548812d5a9c281cbfa5bb0
WingedCryptid <yariksouth@gmail.com>
2023-01-09 18:12:09 +0200

    Added proggresion UI

---
+++ Assets/Scripts/Misc/ProgressionUI.cs
@@ -0,0 +1,24 @@
+﻿using Inventory;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Misc
+{
+    public class ProgressionUI : MonoBehaviour
+    {
+        [SerializeField] private Slider _timeSlider;
+        [SerializeField] private Slider _moneySlider;
+
+        [SerializeField] private PlayerInventory _inventory;
+
+        private void Awake()
+        {
+            _inventory.BalanceChanged += OnMoneyChanged;
+        }
+
+        private void OnMoneyChanged(int balance)
+        {
+            _moneySlider.value = balance;
+        }
+    }
+}

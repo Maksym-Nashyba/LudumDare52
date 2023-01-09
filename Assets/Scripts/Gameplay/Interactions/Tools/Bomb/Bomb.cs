9cfb9f719666a4527526b6aa27fb95576a1b5b46
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2023-01-09 10:21:44 +0100

    Bomb

--- Assets/Scripts/Gameplay/Interactions/Tools/Bomb/Bomb.cs
+++ Assets/Scripts/Gameplay/Interactions/Tools/Bomb/Bomb.cs
@@ -1,13 +1,24 @@
-﻿using Asteroids;
+﻿using System;
+using Asteroids;
 using UnityEngine;
 
 namespace Gameplay.Interactions.Tools
 {
     public class Bomb : Tool
     {
+        [SerializeField] private Interactor _interactor;
+        [SerializeField] private GameObject _placedBombPrefab;
+        private GameObject _placedBomb;
+
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(0) && _placedBomb == null) _interactor.ApplyTool(this); 
+        }
+
         public override void Apply(Asteroid asteroid, Vector3 position)
         {
-            throw new System.NotImplementedException();
+            _placedBomb = Instantiate(_placedBombPrefab);
+            _placedBomb.GetComponent<PlacedBomb>().AttachToAsteroid(asteroid, position);
         }
     }
 }
---
+++ Assets/Scripts/Gameplay/Interactions/Tools/Bomb/PlacedBomb.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Asteroids;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gameplay.Interactions.Tools
+{
+    public class PlacedBomb : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _ticked;
+        [SerializeField] private UnityEvent _exploded;
+        private Asteroid _asteroid;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private void Awake()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        private void OnDestroy()
+        {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+        }
+
+        public void AttachToAsteroid(Asteroid asteroid, Vector3 position)
+        {
+            _asteroid = asteroid;
+            transform.position = position;
+            transform.LookAt(transform.position + (transform.position - asteroid.transform.position));
+            transform.SetParent(asteroid.transform);
+            Charge(2.5f, _cancellationTokenSource.Token);
+        }
+
+        private async Task Charge(float timeSeconds, CancellationToken cancellationToken)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                _ticked?.Invoke();
+                await Task.Delay((int)(timeSeconds/3 * 1000));
+                if(cancellationToken.IsCancellationRequested)return;
+            }
+            Explode();
+        }
+
+        private void Explode()
+        {
+            while (_asteroid != null && !_asteroid.IsDestroyed)
+            {
+                _asteroid.DestroyOuterLayer();
+            }
+            GetComponentInChildren<MeshRenderer>().enabled = false;
+            _exploded?.Invoke();
+            Destroy(this);
+        }
+    }
+}
